8250f6186e58dc75d9c25d6e935a47d7501987dc
thesantatitan <dev19034@iiitd.ac.in>
2020-11-21 23:15:43 +0530

    Refactor CircularObstacle

--- src/model/Obstacle.java
+++ src/model/Obstacle.java
@@ -1,22 +1,7 @@
 package model;
 
-public abstract class Obstacle implements Collidable {
-    private float pos_X;
-    private float pos_Y;
+import javafx.scene.layout.Pane;
 
-    public float getPos_X() {
-        return pos_X;
-    }
+public abstract class Obstacle extends Pane implements Collidable {
 
-    public void setPos_X(float pos_X) {
-        this.pos_X = pos_X;
-    }
-
-    public float getPos_Y() {
-        return pos_Y;
-    }
-
-    public void setPos_Y(float pos_Y) {
-        this.pos_Y = pos_Y;
-    }
 }
