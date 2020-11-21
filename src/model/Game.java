5ed6a3598bf748028ca2744a8dbd43b197619534
thesantatitan <dev19034@iiitd.ac.in>
2020-11-22 00:09:45 +0530

    Revert "Refactor CircularObstacle"
    This reverts commit 2ee81c77

--- src/model/Game.java
+++ src/model/Game.java
@@ -1,35 +1,6 @@
 package model;
 
 
-import view.GameView;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-
 public class Game{
-    private ArrayList<Obstacle> obstacleList;
-    private GameView gameView;
-    private Ball ball;
 
-    private final float HEIGHT = 800;
-    private final float WIDTH = 600;
-
-    public Game(){
-        gameView = new GameView(this);
-        obstacleList = new ArrayList<>();
-        ball = new Ball(WIDTH/2,HEIGHT-2*ball.getRADIUS());
-    }
-
-    public void addObstacle(){
-        obstacleList.add(new CircularObstacle());
-    }
-
-
-    public ArrayList<Obstacle> getObstacleList() {
-        return obstacleList;
-    }
-
-    public Ball getBall() {
-        return ball;
-    }
 }
