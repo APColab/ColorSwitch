c28482187e7e90e73ae2f0c6066df0f3cd1c9648
thesantatitan <dev19034@iiitd.ac.in>
2020-11-22 00:11:11 +0530

    Revert "Refactor CircularObstacle"
    This reverts commit 8250f618

--- src/model/Obstacle.java
+++ src/model/Obstacle.java
@@ -1,7 +1,22 @@
 package model;
 
-import javafx.scene.layout.Pane;
+public abstract class Obstacle implements Collidable {
+    private float pos_X;
+    private float pos_Y;
 
-public abstract class Obstacle extends Pane implements Collidable {
+    public float getPos_X() {
+        return pos_X;
+    }
 
+    public void setPos_X(float pos_X) {
+        this.pos_X = pos_X;
+    }
+
+    public float getPos_Y() {
+        return pos_Y;
+    }
+
+    public void setPos_Y(float pos_Y) {
+        this.pos_Y = pos_Y;
+    }
 }
