ce6a190b14fd0cc7659a5759f3a6afaaca4b8db7
thesantatitan <dev19034@iiitd.ac.in>
2020-12-13 07:36:56 +0530

    Abstract Obstacles

--- src/model/Obstacle.java
+++ src/model/Obstacle.java
@@ -2,38 +2,60 @@ package model;
 
 import javafx.beans.property.FloatProperty;
 import javafx.beans.property.SimpleFloatProperty;
+import view.ObstacleView;
 
 public abstract class Obstacle implements Collidable {
     private FloatProperty pos_X;
     private FloatProperty pos_Y;
+    private final float height = 200;
+    private final float width = 200;
+    private ObstacleView obstacleView;
+
 
     public float getPos_X() {
-        return pos_X.floatValue();
+        return pos_X.get();
     }
 
-    public FloatProperty getPos_XProperty(){
+    public FloatProperty pos_XProperty() {
         return pos_X;
     }
+
     public void setPos_X(float pos_X) {
         if(this.pos_X==null){
-            this.pos_X = new SimpleFloatProperty();
+            this.pos_X = new SimpleFloatProperty(pos_X);
         }
-        this.pos_X.setValue(pos_X);
+        this.pos_X.set(pos_X);
     }
 
     public float getPos_Y() {
-        return pos_Y.floatValue();
+        return pos_Y.get();
     }
 
-    public FloatProperty getPos_YProperty(){
+    public FloatProperty pos_YProperty() {
         return pos_Y;
     }
 
     public void setPos_Y(float pos_Y) {
         if(this.pos_Y==null){
-            this.pos_Y = new SimpleFloatProperty();
+            this.pos_Y = new SimpleFloatProperty(pos_Y);
         }
-        this.pos_Y.setValue(pos_Y);
+        this.pos_Y.set(pos_Y);
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public float getWidth() {
+        return width;
+    }
+
+    public ObstacleView getObstacleView() {
+        return obstacleView;
+    }
+
+    public void setObstacleView(ObstacleView obstacleView) {
+        this.obstacleView = obstacleView;
     }
 
     public abstract void setBindings();
--- src/view/ObstacleView.java
+++ src/view/ObstacleView.java
@@ -1,4 +1,34 @@
 package view;
 
-public abstract class ObstacleView {
+import javafx.animation.Transition;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Shape;
+import model.Obstacle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class ObstacleView extends Pane {
+    private final Obstacle obstacle;
+    private final List<Shape> shapeList;
+    private final Transition transition;
+
+
+    protected ObstacleView(Obstacle obstacle, Transition transition) {
+        this.obstacle = obstacle;
+        this.shapeList = new ArrayList<>();
+        this.transition = transition;
+    }
+
+    public List<Shape> getShapeList() {
+        return shapeList;
+    }
+
+    public Transition getTransition() {
+        return transition;
+    }
+
+    public Obstacle getObstacle() {
+        return obstacle;
+    }
 }
