c94b4358574875259ca18a47e1941efa844bdd6f
thesantatitan <dev19034@iiitd.ac.in>
2020-12-17 05:04:24 +0530

    Serialize RectangularObstacle

--- src/model/RectangularObstacle.java
+++ src/model/RectangularObstacle.java
@@ -2,11 +2,17 @@ package model;
 
 import javafx.beans.property.FloatProperty;
 import javafx.beans.property.SimpleFloatProperty;
+import view.CrossObstacleView;
 import view.RectangularObstacleView;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serial;
+
 public class   RectangularObstacle extends Obstacle {
-    private FloatProperty length;
-    private FloatProperty breadth;
+    private transient FloatProperty length;
+    private transient FloatProperty breadth;
 
     public RectangularObstacle(float pos_x, float pos_y){
         this.setPos_X(pos_x);
@@ -28,6 +34,26 @@ public class   RectangularObstacle extends Obstacle {
     public void setBindings() {
         this.pos_XProperty().bindBidirectional(this.getObstacleView().layoutXProperty());
         this.pos_YProperty().bindBidirectional(this.getObstacleView().layoutYProperty());
+    }
+
+    @Serial
+    private void writeObject(ObjectOutputStream ous) throws IOException {
+        ous.defaultWriteObject();
+        ous.writeFloat(getPos_X());
+        ous.writeFloat(getPos_Y());
+        ous.writeFloat(getLength());
+        ous.writeFloat(getBreadth());
+    }
+
+    @Serial
+    private void readObject(ObjectInputStream ois) throws ClassNotFoundException,IOException{
+        ois.defaultReadObject();
+        this.setPos_X(ois.readFloat());
+        this.setPos_Y(ois.readFloat());
+        this.length = new SimpleFloatProperty(ois.readFloat());
+        this.length = new SimpleFloatProperty(ois.readFloat());
+        setObstacleView(new RectangularObstacleView(this));
+        setBindings();
     }
 
 
