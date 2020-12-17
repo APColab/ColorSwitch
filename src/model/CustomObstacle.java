ae3a173a58847b1eda0ba8c1ef4a75161216ec4c
thesantatitan <dev19034@iiitd.ac.in>
2020-12-17 19:10:44 +0530

    Add ObstacleCreator

---
+++ src/model/CustomObstacle.java
@@ -0,0 +1,13 @@
+package model;
+
+public class CustomObstacle extends Obstacle{
+    @Override
+    public void handleCollision() {
+
+    }
+
+    @Override
+    public void setBindings() {
+
+    }
+}
