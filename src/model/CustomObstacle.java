365033a03bc7fbc30c59336da4dd3901c0b0da00
thesantatitan <dev19034@iiitd.ac.in>
2020-12-17 20:34:45 +0530

    Add CustomObstacleLoader, CustomObstacle

--- src/model/CustomObstacle.java
+++ src/model/CustomObstacle.java
@@ -1,6 +1,76 @@
 package model;
 
+import javafx.beans.property.SimpleFloatProperty;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.util.Duration;
+import view.CircularObstacleView;
+import view.CrossObstacleView;
+import view.CustomObstacleView;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serial;
+import java.util.ArrayList;
+
 public class CustomObstacle extends Obstacle{
+
+    private ArrayList<Line> lines;
+
+    public CustomObstacle(float pos_x, float pos_y,ArrayList<Line> lines){
+        this.setPos_X(pos_x);
+        this.setPos_Y(pos_y);
+        this.lines = new ArrayList<>();
+        this.lines.addAll(lines);
+        setObstacleView(new CustomObstacleView(this,lines));
+        setBindings();
+    }
+
+
+    @Serial
+    private void writeObject(ObjectOutputStream ous) throws IOException {
+        ous.defaultWriteObject();
+        ous.writeFloat(getPos_X());
+        ous.writeFloat(getPos_Y());
+        for(Line l:lines){
+            ous.writeDouble(l.getStartX());
+            ous.writeDouble(l.getStartY());
+            ous.writeDouble(l.getEndX());
+            ous.writeDouble(l.getEndY());
+        }
+        ous.writeDouble(getObstacleView().getTransition().getCurrentTime().toMillis());
+    }
+
+    @Serial
+    private void readObject(ObjectInputStream ois) throws ClassNotFoundException,IOException{
+        ois.defaultReadObject();
+        this.setPos_X(ois.readFloat());
+        this.setPos_Y(ois.readFloat());
+        lines = new ArrayList<>();
+        for(int i=0;i<4;i++){
+            Line line = new Line(ois.readDouble(),ois.readDouble(),ois.readDouble(),ois.readDouble());
+            switch(i){
+                case 0->{
+                    line.setStroke(Color.RED);
+                }
+                case 1->{
+                    line.setStroke(Color.GREEN);
+                }
+                case 2->{
+                    line.setStroke(Color.BLUE);
+                }
+                case 3->{
+                    line.setStroke(Color.YELLOW);
+                }
+            }
+            lines.add(line);
+        }
+        setObstacleView(new CustomObstacleView(this,lines));
+        setBindings();
+        getObstacleView().getTransition().jumpTo(Duration.millis(ois.readDouble()));
+    }
+
     @Override
     public void handleCollision() {
 
@@ -8,6 +78,7 @@ public class CustomObstacle extends Obstacle{
 
     @Override
     public void setBindings() {
-
+        this.pos_XProperty().bindBidirectional(this.getObstacleView().layoutXProperty());
+        this.pos_YProperty().bindBidirectional(this.getObstacleView().layoutYProperty());
     }
 }
--- src/view/CustomObstacleView.java
+++ src/view/CustomObstacleView.java
@@ -1,4 +1,35 @@
 package view;
 
-public class CustomObstacleView{
+import javafx.animation.Animation;
+import javafx.animation.Interpolator;
+import javafx.animation.RotateTransition;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.util.Duration;
+import model.CrossObstacle;
+import model.CustomObstacle;
+
+import java.util.ArrayList;
+
+public class CustomObstacleView extends ObstacleView{
+
+    public CustomObstacleView(CustomObstacle c, ArrayList<Line> lines){
+        super(c,new RotateTransition(Duration.millis(3000)));
+        getShapeList().addAll(lines);
+        for(int i=0;i<4;i++){
+            getShapeList().get(i).setStrokeWidth(c.getStroke()+3);
+        }
+        this.setPrefHeight(getObstacle().getHeight());
+        this.setPrefWidth(getObstacle().getWidth());
+        this.setLayoutX(getObstacle().getPos_X());
+        this.setLayoutY(getObstacle().getPos_Y());
+        this.getChildren().addAll(getShapeList());
+
+        ((RotateTransition)getTransition()).setNode(this);
+        ((RotateTransition)getTransition()).setCycleCount(Animation.INDEFINITE);
+        ((RotateTransition)getTransition()).setByAngle(360);
+        ((RotateTransition)getTransition()).setInterpolator(Interpolator.LINEAR);
+        ((RotateTransition)getTransition()).play();
+    }
+
 }
